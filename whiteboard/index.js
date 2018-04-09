154c35a609c5df456ab21ef7a2770c215ae648ca
vishal-pandey <pandeyvishal25@yahoo.com>
2018-04-09 17:53:59 +0530

    port change

--- whiteboard/index.js
+++ whiteboard/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3210;
 
 app.use(express.static(__dirname + '/public'));
 
